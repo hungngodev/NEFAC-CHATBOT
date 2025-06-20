d1c79541aea6ca0bec38800b33bfa461cc616ee5
HUNG NGO <hungngo1607@gmail.com>
2025-06-20 15:05:14 -0700

    add

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -19,6 +19,7 @@ module.exports = {
   },
   plugins: ['react-refresh'],
   rules: {
+    'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
